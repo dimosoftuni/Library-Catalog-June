5456cf5b7c4ae362c7eae2013d2c7e15e679792a
Dimo Mitev <dimosoftuni@gmail.com>
2024-05-30 21:10:16 +0300

    add login user tests

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -44,3 +44,28 @@ test('Verify Register link text', async ({ page }) => {
 
     expect(registerLinkText).toEqual("Register");
 });
+
+test('Verify valid user can login', async ({ page }) => {
+    await page.goto(appUrl);
+    await page.waitForSelector('nav.navbar');
+
+    // Navigate to Login page
+    const loginLink = await page.$('a[href="/login"]');
+    await loginLink.click();
+
+    // Fill the user data
+    await page.fill('#email', "peter@abv.bg");
+    await page.fill('#password', "123456");
+
+    // Click on Login button
+    const loginButton = await page.$('xpath=//*[@id="login-form"]/fieldset/input');
+    await loginButton.click();
+
+    // Verify the Logout button is present
+    // await page.waitForURL('http://localhost:3000/catalog');
+    const logoutButton = await page.$('#logoutBtn');
+    const logoutButtonText = await logoutButton.textContent();
+
+    expect(logoutButtonText).toEqual("Logout");
+
+});
