490a84554e0e67b4d85951e470425ab89c826465
Dimo Mitev <dimosoftuni@gmail.com>
2024-05-30 20:50:46 +0300

    Add tests for link validation

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,8 +1,9 @@
 const { test, expect } = require("@playwright/test");
+const appUrl = "http://localhost:3000"
 
 test('Verify All Books link is visible', async ({ page }) => {
     // Open the applicaiton
-    await page.goto("http://localhost:3000");
+    await page.goto(appUrl);
 
     // locate page toolbar
     await page.waitForSelector('nav.navbar');
@@ -15,5 +16,31 @@ test('Verify All Books link is visible', async ({ page }) => {
 
     // Verify the element is visible
     expect(isElementVisible).toBe(true);
+});
 
+test('Verify Login link is visible', async ({ page }) => {
+    await page.goto(appUrl);
+    await page.waitForSelector('nav.navbar');
+    const loginLink = await page.$('a[href="/login"]');
+    const isElementVisible = await loginLink.isVisible();
+
+    expect(isElementVisible).toBe(true);
+});
+
+test('Verify Register link is visible', async ({ page }) => {
+    await page.goto(appUrl);
+    await page.waitForSelector('nav.navbar');
+    const registerLink = await page.$('a[href="/register"]');
+    const isElementVisible = await registerLink.isVisible();
+
+    expect(isElementVisible).toBe(true);
+});
+
+test('Verify Register link text', async ({ page }) => {
+    await page.goto(appUrl);
+    await page.waitForSelector('nav.navbar');
+    const registerLink = await page.$('a[href="/register"]');
+    const registerLinkText = await registerLink.textContent();
+
+    expect(registerLinkText).toEqual("Register");
 });
